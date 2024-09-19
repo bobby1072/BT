0a67c95a70bae2c87e10b19cebd93dd1e4f68348
Bobby Thompson <bobbythompson@paypoint.com>
2024-09-20 00:38:36 +0100

    Built array fast select

---
+++ src/BT.Common/BT.Common.FastApi/Proto/FastArraySelect.cs
@@ -0,0 +1,23 @@
+﻿namespace BT.Common.FastArray.Proto
+{
+    public static partial class FastArray
+    {
+        public static IEnumerable<TNew> FastArraySelect<TOriginal, TNew>(this IEnumerable<TOriginal> values, Func<TOriginal, TNew> transformFunc)
+        {
+            foreach (var value in values)
+            {
+                yield return transformFunc.Invoke(value);
+            }
+        }
+        public static IEnumerable<TNew> FastArraySelectWhere<TOriginal, TNew>(this IEnumerable<TOriginal> values, Func<TOriginal, bool> predicate, Func<TOriginal, TNew> transformFunc)
+        {
+            foreach (var value in values)
+            {
+                if (predicate.Invoke(value))
+                {
+                    yield return transformFunc(value);
+                }
+            }
+        }
+    }
+}
