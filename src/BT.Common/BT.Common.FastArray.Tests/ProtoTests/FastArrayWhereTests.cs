703246e0fe164cc1065d29ca40774d3e4feb94b3
bobby1072 <bobby.thompson1072@gmail.com>
2024-09-25 00:41:20 +0100

    More tests addded

--- src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayWhereTests.cs
+++ src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayWhereTests.cs
@@ -1,4 +1,5 @@
 ﻿using BT.Common.FastArray.Proto;
+using BT.Common.FastArray.Tests.Models;
 
 namespace BT.Common.FastArray.Tests.ProtoTests
 {
@@ -15,6 +16,13 @@ namespace BT.Common.FastArray.Tests.ProtoTests
                     Add(arrayData, x => x.Where(y => y is not null), x => x.FastArrayWhere(y => y is not null));
                     Add(arrayData, x => x.Where(y => y is true), x => x.FastArrayWhere(y => y is true));
                     Add(arrayData, x => x.Where(y => y is false), x => x.FastArrayWhere(y => y is false));
+                    Add(arrayData, x => x.Where(y => y is int), x => x.FastArrayWhere(y => y is int));
+                    Add(arrayData, x => x.Where(y => y is int && (int)y > 100), x => x.FastArrayWhere(y => y is int && (int)y > 100));
+                    Add(arrayData, x => x.Where(y => y is int && (int)y > 1000), x => x.FastArrayWhere(y => y is int && (int)y > 1000));
+                    Add(arrayData, x => x.Where(y => y is string), x => x.FastArrayWhere(y => y is string));
+                    Add(arrayData, x => x.Where(y => y is TestVehicle), x => x.FastArrayWhere(y => y is TestVehicle));
+                    Add(arrayData, x => x.Where(y => y is TestCar), x => x.FastArrayWhere(y => y is TestCar));
+                    Add(arrayData, x => x.Where(y => y is TestPlane), x => x.FastArrayWhere(y => y is TestPlane));
                 }
             }
         }
