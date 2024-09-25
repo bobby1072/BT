a7fa9b60171816689d06e85c2971cf7f5dcba789
bobby1072 <bobby.thompson1072@gmail.com>
2024-09-25 01:42:23 +0100

    More tests

--- src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArraySelectTests.cs
+++ src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArraySelectTests.cs
@@ -1,4 +1,5 @@
 using BT.Common.FastArray.Proto;
+using BT.Common.FastArray.Tests.TestModels;
 
 namespace BT.Common.FastArray.Tests.ProtoTests
 {
@@ -19,6 +20,34 @@ namespace BT.Common.FastArray.Tests.ProtoTests
         [Theory]
         [ClassData(typeof(FastArraySelectTests_Functionality_Class_Data))]
         public void FastArraySelectTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
+        {
+            FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
+        }
+        private class FastArraySelectWhereTests_Functionality_Class_Data : TheoryData<IReadOnlyCollection<object>, Func<IEnumerable<object>, IEnumerable<object>>, Func<IEnumerable<object>, IEnumerable<object>>>
+        {
+            public FastArraySelectWhereTests_Functionality_Class_Data()
+            {
+                Add(new List<object> { 1, 2, 3 }, x => x.Select(y => y).Where(x => x is int), x => x.FastArraySelectWhere(x => x is int, x => x));
+
+                foreach (var arrayData in _basicArraysToTestFunctionality)
+                {
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is not null), x => x.FastArraySelectWhere(y => y is not null, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is true), x => x.FastArraySelectWhere(y => y is true, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is false), x => x.FastArraySelectWhere(y => y is false, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is int), x => x.FastArraySelectWhere(y => y is int, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is int && (int)y > 100), x => x.FastArraySelectWhere(y => y is int && (int)y > 100, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is int && (int)y > 1000), x => x.FastArraySelectWhere(y => y is int && (int)y > 1000, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is string), x => x.FastArraySelectWhere(y => y is string, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is TestVehicle), x => x.FastArraySelectWhere(y => y is TestVehicle, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is TestCar), x => x.FastArraySelectWhere(y => y is TestCar, y => y));
+                    Add(arrayData, x => x.Select(x => x).Where(y => y is TestPlane), x => x.FastArraySelectWhere(y => y is TestPlane, y => y));
+
+                }
+            }
+        }
+        [Theory]
+        [ClassData(typeof(FastArraySelectWhereTests_Functionality_Class_Data))]
+        public void FastArraySelectWhereTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
         {
             FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
         }
