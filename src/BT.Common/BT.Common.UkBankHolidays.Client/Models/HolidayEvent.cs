06d0059afccaad526c8676c2925e734b88e8081a
bobby1072 <bobby.thompson1072@gmail.com>
2024-09-29 02:51:39 +0100

    Created

---
+++ src/BT.Common/BT.Common.UkBankHolidays.Client/Models/HolidayEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BT.Common.UkHoliday.Client.Models
+{
+    public record HolidayEvent
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; init; }
+        [JsonPropertyName("date")]
+        public DateOnly Date { get; init; }
+        [JsonPropertyName("notes")]
+        public string Notes { get; init; }
+        [JsonPropertyName("bunting")]
+        public bool Bunting { get; init; }
+    }
+}
---
+++ src/BT.Common/BT.Common.UkBankHolidays.Client/Models/UkHolidays.cs
@@ -0,0 +1,15 @@
+﻿using BT.Common.UkHoliday.Client.Models;
+using System.Text.Json.Serialization;
+
+namespace BT.Common.UkBankHolidays.Client.Models
+{
+    public record UkHolidays
+    {
+        [JsonPropertyName("englandandwales")]
+        public CountryEvents EnglandAndWales { get; init; }
+        [JsonPropertyName("scotland")]
+        public CountryEvents Scotland { get; init; }
+        [JsonPropertyName("northernireland")]
+        public CountryEvents NorthernIreland { get; init; }
+    }
+}
