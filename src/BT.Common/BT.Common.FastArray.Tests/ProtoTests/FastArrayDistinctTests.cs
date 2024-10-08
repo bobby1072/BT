36c1a8b1eaea74327842f81fb047d95179f8eace
bobby1072 <bobby.thompson1072@gmail.com>
2024-10-08 23:47:21 +0100

    Needs work

--- src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayDistinctTests.cs
+++ src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayDistinctTests.cs
@@ -1,5 +1,4 @@
 using BT.Common.FastArray.Proto;
-using BT.Common.FastArray.Tests.TestModels;
 
 namespace BT.Common.FastArray.Tests.ProtoTests
 {
@@ -9,17 +8,71 @@ namespace BT.Common.FastArray.Tests.ProtoTests
         {
             public FastArrayDistinctTests_Functionality_Class_Data()
             {
-                Add(new List<object> { 1, 2, 3 }, x => x.Distinct(), x => x.Distinct());
+                Add(new List<object> { 1, 2, 3 }, x => x.Distinct(), x => x.FastArrayDistinct());
 
                 foreach (var arrayData in _basicArraysToTestFunctionality)
                 {
-                    Add(arrayData, x => x.Distinct(), x => x.Distinct());
+                    Add(arrayData, x => x.Distinct(), x => x.FastArrayDistinct());
                 }
             }
         }
         [Theory]
         [ClassData(typeof(FastArrayDistinctTests_Functionality_Class_Data))]
         public void FastArrayDistinctTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
+        {
+            FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
+        }
+        private class FastArrayDistinctByTests_Functionality_Class_Data : TheoryData<IReadOnlyCollection<object>, Func<IEnumerable<object>, IEnumerable<object>>, Func<IEnumerable<object>, IEnumerable<object>>>
+        {
+            private class TestIsStringEqualityComparer : IEqualityComparer<object>
+            {
+                public new bool Equals(object? x, object? y)
+                {
+                    return x is string && y is string && x == y;
+                }
+                public int GetHashCode(object obj)
+                {
+                    return obj.GetHashCode();
+                }
+            }
+            private class TestIsNullEqualityComparer : IEqualityComparer<object>
+            {
+                public new bool Equals(object? x, object? y)
+                {
+                    return x == null && y == null;
+                }
+                public int GetHashCode(object obj)
+                {
+                    return obj.GetHashCode();
+                }
+            }
+            private class TestIsIntAndXGreaterThanYEqualityComparer : IEqualityComparer<object>
+            {
+                public new bool Equals(object? x, object? y)
+                {
+                    return x is int foundX && y is int foundY && foundX > foundY;
+                }
+                public int GetHashCode(object obj)
+                {
+                    return obj.GetHashCode();
+                }
+            }
+            public FastArrayDistinctByTests_Functionality_Class_Data()
+            {
+                Add(new List<object> { 1, 2, 3 }, x => x.DistinctBy(x => x), x => x.FastArrayDistinctBy(x => x));
+
+                foreach (var arrayData in _basicArraysToTestFunctionality)
+                {
+                    Add(arrayData, x => x.DistinctBy(x => x), x => x.FastArrayDistinctBy(x => x));
+                    Add(arrayData, x => x.DistinctBy(x => x, new TestIsStringEqualityComparer()), x => x.FastArrayDistinctBy(x => x, (x, y) => x is string && y is string && x == y));
+                    Add(arrayData, x => x.DistinctBy(x => x, new TestIsNullEqualityComparer()), x => x.FastArrayDistinctBy(x => x, (x, y) => x == null && y == null));
+                    Add(arrayData, x => x.DistinctBy(x => x, new TestIsIntAndXGreaterThanYEqualityComparer()), x => x.FastArrayDistinctBy(x => x, (x, y) => x is int foundX && y is int foundY && foundX > foundY));
+                }
+            }
+        }
+        [Theory]
+        [ClassData(typeof(FastArrayDistinctByTests_Functionality_Class_Data))]
+        public void FastArrayDistinctByTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
         {
             FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
         }
--- src/BT.Common/BT.Common.FastArray/Proto/FastArrayDistinct.cs
+++ src/BT.Common/BT.Common.FastArray/Proto/FastArrayDistinct.cs
@@ -13,5 +13,37 @@ namespace BT.Common.FastArray.Proto
                 }
             }
         }
+        public static IEnumerable<T> FastArrayDistinctBy<T, TKey>(this IEnumerable<T> values, Func<T, TKey> keySelector, Func<TKey, TKey, bool>? comparer = null)
+        {
+            var seenKeys = new HashSet<TKey>();
+            foreach (var value in values)
+            {
+                var key = keySelector.Invoke(value);
+                if (comparer == null)
+                {
+                    if (seenKeys.Add(key))
+                    {
+                        yield return value;
+                    }
+                }
+                else
+                {
+                    var found = false;
+                    foreach (var seenKey in seenKeys)
+                    {
+                        if (comparer.Invoke(seenKey, key))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        seenKeys.Add(key);
+                        yield return value;
+                    }
+                }
+            }
+        }
     }
 }
