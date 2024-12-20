0e846d38f22aca07f1277fb812c7fd93771f8807
bobby1072 <bobby.thompson1072@gmail.com>
2024-12-20 23:48:10 +0000

    ASmall

--- src/BT.Common/BT.Common.HttpClient/Extensions/HttpClientExtensions.cs
+++ src/BT.Common/BT.Common.HttpClient/Extensions/HttpClientExtensions.cs
@@ -4,11 +4,17 @@ namespace BT.Common.HttpClient.Extensions;
 
 public static class HttpClientExtensions
 {
-    public static async Task<HttpResponseMessage> SendAsync(this System.Net.Http.HttpClient client, HttpRequestMessage request,
-        PollyRetrySettings retrySettings)
+    public static async Task<HttpResponseMessage> SendAsync(
+        this System.Net.Http.HttpClient client,
+        HttpRequestMessage request,
+        PollyRetrySettings retrySettings,
+        CancellationToken cancellationToken = default
+    )
     {
         var pipeline = retrySettings.ToPipeline();
-        
-        return await pipeline.ExecuteAsync(async ct => await client.SendAsync(request, ct));
+
+        return await pipeline.ExecuteAsync(async ct =>
+            await client.SendAsync(request, cancellationToken)
+        );
     }
 }
