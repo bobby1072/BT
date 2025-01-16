6c68a9ca24a35f07d020314c3c09970a094b9e6f
bobby1072 <bobby.thompson1072@gmail.com>
2025-01-16 21:09:52 +0000

    Added a couple extensions

---
+++ src/BT.Common/BT.Common.Helpers/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace BT.Common.Helpers.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static T? GetPropertyValue<T>(this object? value, string propertyName)
+        {
+            if (value is null)
+            {
+                return default;
+            }
+
+            var valType = value.GetType();
+            var correctProperties = valType.GetProperties().SingleOrDefault(x => x.Name == propertyName && x.PropertyType == typeof(T));
+
+            return correctProperties?.GetValue(value) is T result ? result : default;
+        }
+    }
+}
