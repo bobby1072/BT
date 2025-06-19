9ef6380bb0e03d08885967e79758133f43f2ddd8
Bobby Thompson <bobbythompson@paypoint.com>
2025-06-19 07:34:04 +0100

    Clean up

--- src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayFirstTests.cs
+++ src/BT.Common/BT.Common.FastArray.Tests/ProtoTests/FastArrayFirstTests.cs
@@ -5,9 +5,9 @@ namespace BT.Common.FastArray.Tests.ProtoTests
 {
     public class FastArrayFirstTests : FastArrayTestBase
     {
-        private class FastArrayWhereTestsFunctionalityClassData : TheoryData<IReadOnlyCollection<object>, Func<IEnumerable<object>, IEnumerable<object>>, Func<IEnumerable<object>, IEnumerable<object>>>
+        private class FastArrayFirstOrDefaultTestsFunctionalityClassData : TheoryData<IReadOnlyCollection<object>, Func<IEnumerable<object>, IEnumerable<object>>, Func<IEnumerable<object>, IEnumerable<object>>>
         {
-            public FastArrayWhereTestsFunctionalityClassData()
+            public FastArrayFirstOrDefaultTestsFunctionalityClassData()
             {
                 Add(new List<object> { 1, 2, 3 }, x => [x.FirstOrDefault(y => y is int)!], x => [x.FastArrayFirstOrDefault(y => y is int)!]);
 
@@ -27,10 +27,39 @@ namespace BT.Common.FastArray.Tests.ProtoTests
             }
         }
         [Theory]
-        [ClassData(typeof(FastArrayWhereTestsFunctionalityClassData))]
-        public void FastArrayWhereTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
+        [ClassData(typeof(FastArrayFirstOrDefaultTestsFunctionalityClassData))]
+        public void FastArrayFirstOrDefaultTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
         {
             FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
         }
+        private class FastArrayFirstTestsFunctionalityClassData : TheoryData<IReadOnlyCollection<object>, Func<IEnumerable<object>, IEnumerable<object>>, Func<IEnumerable<object>, IEnumerable<object>>>
+        {
+            public FastArrayFirstTestsFunctionalityClassData()
+            {
+                Add(new List<object> { 1, 2, 3 }, x => [x.FirstOrDefault(y => y is int)!], x => [x.FastArrayFirst(y => y is int)!]);
+
+                foreach (var arrayData in BasicArraysToTestFunctionality)
+                {
+                    Add(arrayData, x => [x.First(y => y is not null)!], x => [x.FastArrayFirst(y => y is not null)!]);
+                    Add(arrayData, x => [x.First(y => y is true)!], x => [x.FastArrayFirst(y => y is true)!]);
+                    Add(arrayData, x => [x.First(y => y is false)!], x => [x.FastArrayFirst(y => y is false)!]);
+                    Add(arrayData, x => [x.First(y => y is int)!], x => [x.FastArrayFirst(y => y is int)!]);
+                    Add(arrayData, x => [x.First(y => y is int && (int)y > 100)!], x => [x.FastArrayFirst(y => y is int && (int)y > 100)!]);
+                    Add(arrayData, x => [x.First(y => y is int && (int)y > 1000)!], x => [x.FastArrayFirst(y => y is int && (int)y > 1000)!]);
+                    Add(arrayData, x => [x.First(y => y is string)!], x => [x.FastArrayFirst(y => y is string)!]);
+                    Add(arrayData, x => [x.First(y => y is TestVehicle)!], x => [x.FastArrayFirst(y => y is TestVehicle)!]);
+                    Add(arrayData, x => [x.First(y => y is TestCar)!], x => [x.FastArrayFirst(y => y is TestCar)!]);
+                    Add(arrayData, x => [x.First(y => y is TestPlane)!], x => [x.FastArrayFirst(y => y is TestPlane)!]);
+                }
+            }
+        }
+        
+        [Theory]
+        [ClassData(typeof(FastArrayFirstTestsFunctionalityClassData))]
+        public void FastArrayFirstTests_Functionality(IReadOnlyCollection<object> arrayData, Func<IEnumerable<object>, IEnumerable<object>> actualFunc, Func<IEnumerable<object>, IEnumerable<object>> yourFunc)
+        {
+            FunctionalityTestRunner(arrayData, actualFunc, yourFunc);
+        }
+
     }
 }
--- src/BT.Common/BT.Common.FastArray/Proto/FastArrayFirst.cs
+++ src/BT.Common/BT.Common.FastArray/Proto/FastArrayFirst.cs
@@ -1,12 +1,20 @@
+using System.Text.Json;
+
 namespace BT.Common.FastArray.Proto
 {
     public static partial class FastArray
     {
-        public static T? FastArrayFirstOrDefault<T>(this IEnumerable<T> values, Func<T, bool> predicate)
+        public static T FastArrayFirst<T>(this IEnumerable<T> values, Func<T, bool>? predicate = null)
+        {
+            return values.FastArrayFirstOrDefault(predicate) ?? throw new NullReferenceException();
+        }
+        
+        
+        public static T? FastArrayFirstOrDefault<T>(this IEnumerable<T> values, Func<T, bool>? predicate = null)
         {
             foreach (var value in values)
             {
-                if (predicate.Invoke(value))
+                if (predicate is null || predicate.Invoke(value))
                 {
                     return value;
                 }
