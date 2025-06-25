d2a925dcac1f1c6aaf760db58f77bc892af1bf27
bobby1072 <bobby.thompson1072@gmail.com>
2025-06-26 00:15:34 +0100

    Added it

---
+++ src/BT.Common/BT.Common.Http.TestBase/TestHttpClient.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace BT.Common.Http.TestBase;
+
+public sealed class TestHttpClient: HttpClient
+{
+    private string? _actualUri;
+    private Dictionary<string, string?> _acutalHeaders = [];
+    private HttpMethod? _actualMethod;
+    public TestHttpClient(HttpMessageHandler handler) : base(handler)
+    { }
+
+    public void WasExpectedUrlCalled(string expectedUrl)
+    {
+        Assert.Equal(expectedUrl, _actualUri);
+    }
+
+    public void WasExpectedHeaderCalled(string headerName)
+    {
+        Assert.Contains(headerName, _acutalHeaders);
+    }
+
+    public void WasExpectedHeaderCalled(string headerName, string headerValue)
+    {
+        Assert.Single(_acutalHeaders, pair => headerName == pair.Key && headerValue == pair.Value);
+    }
+
+    public void WasExpectedHttpMethodUsed(HttpMethod method)
+    {
+        Assert.Equal(_actualMethod, method);
+    }
+
+    public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        _actualUri = request.RequestUri?.ToString();
+        _acutalHeaders = request.Headers.ToDictionary(x => x.Key, x => x.Value.FirstOrDefault());
+        _actualMethod = request.Method;
+        
+        return base.SendAsync(request, cancellationToken);
+    }
+}
---
+++ src/BT.Common/BT.Common.Http.TestBase/TestStaticJsonHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace BT.Common.Http.TestBase;
+
+public sealed class TestStaticJsonHandler<T> : HttpMessageHandler
+    where T : class
+{
+    private readonly T _json;
+    private readonly HttpStatusCode _statusCode;
+    private readonly JsonSerializerOptions? _jsonSerializerOptions;
+
+    public TestStaticJsonHandler(
+        T data,
+        HttpStatusCode statusCode,
+        JsonSerializerOptions? jsonSerializerOptions = null
+    )
+    {
+        _json = data;
+        _statusCode = statusCode;
+        _jsonSerializerOptions = jsonSerializerOptions;
+    }
+
+    protected sealed override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        var response = new HttpResponseMessage()
+        {
+            StatusCode = _statusCode,
+            Content = new StringContent(
+                JsonSerializer.Serialize(_json, _jsonSerializerOptions),
+                Encoding.UTF8,
+                "application/json"
+            ),
+        };
+
+        return Task.FromResult(response);
+    }
+}
