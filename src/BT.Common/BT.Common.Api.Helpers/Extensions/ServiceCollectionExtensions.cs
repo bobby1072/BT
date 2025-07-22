0e63699c0e8817d250ac6bffb6dc933647e6090b
Bobby Thompson <bobbythompson@paypoint.com>
2025-07-22 11:08:24 +0100

    Have added things

---
+++ src/BT.Common/BT.Common.Api.Helpers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace BT.Common.Api.Helpers.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureSingletonOptions<TOptions>(this IServiceCollection services,
+        IConfiguration configSection) where TOptions : class
+    {
+        services
+            .Configure<TOptions>(configSection)
+            .AddSingleton<TOptions>(sp => sp.GetRequiredService<IOptions<TOptions>>().Value);
+
+        return services;
+    }
+    public static IServiceCollection ConfigureScopedOptions<TOptions>(this IServiceCollection services,
+        IConfiguration configSection) where TOptions : class
+    {
+        services
+            .Configure<TOptions>(configSection)
+            .AddScoped<TOptions>(sp => sp.GetRequiredService<IOptionsSnapshot<TOptions>>().Value);
+
+        return services;
+    }
+}
