f60ef0d93b1c7a04e2f9c076328044d3f420a483
Bobby Thompson <bobbythompson@paypoint.com>
2025-07-22 11:17:39 +0100

    Move helpers class

--- src/BT.Common/BT.Common.Helpers/Extensions/ServiceCollectionExtensions.cs
+++ src/BT.Common/BT.Common.Helpers/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
-namespace BT.Common.Api.Helpers.Extensions;
+namespace BT.Common.Helpers.Extensions;
 
 public static class ServiceCollectionExtensions
 {
