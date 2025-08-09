20ae97b879bbd3cf2c0b5a303514855cdd23d751
bobby1072 <bobby.thompson1072@gmail.com>
2025-08-10 00:30:25 +0100

    Done

--- src/BT.Common/BT.Common.Helpers/LoggingHelper.cs
+++ src/BT.Common/BT.Common.Helpers/LoggingHelper.cs
@@ -8,21 +8,18 @@ public static class LoggingHelper
 {
     public static ILogger CreateLogger()
     {
-        using var loggerFactory = LoggerFactory.Create(logBuilder=> logBuilder.AddJsonConsole(ConfigureJsonConsole));
-        
+        using var loggerFactory = LoggerFactory.Create(logBuilder =>
+            logBuilder.AddJsonConsole(ConfigureJsonConsole)
+        );
+
         var logger = loggerFactory.CreateLogger<WebApplicationBuilder>();
 
         return logger;
     }
-    
+
     private static void ConfigureJsonConsole(JsonConsoleFormatterOptions options)
     {
         options.IncludeScopes = true;
-        options.TimestampFormat = "yyyy-MM-dd HH:mm:ss";
         options.UseUtcTimestamp = true;
-        options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
-        {
-            Indented = false,
-        };
     }
 }
