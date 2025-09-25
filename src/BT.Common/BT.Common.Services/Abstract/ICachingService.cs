1da270cd35bc9fb48fc641afa35ef6b08375aae7
bobby1072 <bobby.thompson1072@gmail.com>
2025-09-25 22:59:03 +0100

    Added

--- src/BT.Common/BT.Common.Services/Abstract/ICachingService.cs
+++ src/BT.Common/BT.Common.Services/Abstract/ICachingService.cs
@@ -6,14 +6,14 @@ namespace BT.Common.Services.Abstract;
 
 public interface ICachingService
 {
-    Task<T?> TryGetObjectAsync<T>(string key)
+    Task<T?> TryGetObjectAsync<T>(string key, CancellationToken cancellationToken = default)
         where T : class;
     Task<string> SetObjectAsync<T>(
         string key,
         T value,
-        CacheObjectTimeToLiveInSeconds timeToLive = CacheObjectTimeToLiveInSeconds.TenMinutes
+        CacheObjectTimeToLiveInSeconds timeToLive = CacheObjectTimeToLiveInSeconds.TenMinutes, CancellationToken cancellationToken = default
     )
         where T : class;
-    Task<string> SetObjectAsync<T>(string key, T value, DistributedCacheEntryOptions options)
+    Task<string> SetObjectAsync<T>(string key, T value, DistributedCacheEntryOptions options, CancellationToken cancellationToken = default)
         where T : class;
 }
