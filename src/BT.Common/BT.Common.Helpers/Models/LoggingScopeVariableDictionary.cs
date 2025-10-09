8963a7610782792b98a8daf4333c2ac8d0562ffb
BobbyThompson <bobbythompson@paypoint.com>
2025-10-09 13:21:54 +0100

    Done it

---
+++ src/BT.Common/BT.Common.Helpers/Models/LoggingScopeVariableDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace BT.Common.Helpers.Models;
+
+public sealed class LoggingScopeVariableDictionary: Dictionary<string, object>
+{
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
