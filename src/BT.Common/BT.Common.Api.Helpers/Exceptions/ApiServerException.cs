9702c721b886803d5ee5827333c0af175fa1d43f
bobby1072 <bobby.thompson1072@gmail.com>
2025-10-16 23:07:24 +0100

    added them

---
+++ src/BT.Common/BT.Common.Api.Helpers/Exceptions/ApiServerException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace BT.Common.Api.Helpers.Exceptions;
+
+public sealed class ApiServerException: ApiException
+{
+    public ApiServerException(string message) : base(LogLevel.Error, HttpStatusCode.InternalServerError, message) { }
+    public ApiServerException(HttpStatusCode statusCode, string message) : base(LogLevel.Error, statusCode, message) { }
+    public ApiServerException(LogLevel logLevel, HttpStatusCode statusCode, string message) : base(logLevel, statusCode, message) { }
+}
