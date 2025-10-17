1f7a395b0f8d86ae2df7f1f1047da09f5ba2f093
BobbyThompson <bobbythompson@paypoint.com>
2025-10-17 18:37:32 +0100

    Alreadsy added

---
+++ src/BT.Common/BT.Common.Helpers/EnumHelper.cs
@@ -0,0 +1,26 @@
+﻿using BT.Common.Helpers.Extensions;
+
+namespace BT.Common.Helpers;
+
+public static class EnumHelper
+{
+    public static TEnum Parse<TEnum>(string objToParse) where TEnum : struct, Enum
+    {
+        var foundDisplayMap = Enum.GetValues<TEnum>().Select(x =>
+        new {
+            Num = x, DisName = x.GetDisplayName()
+        }).FirstOrDefault(x => x.DisName == objToParse);
+        
+        if (foundDisplayMap is not null)
+        {
+            return foundDisplayMap.Num;
+        }
+        
+        return Enum.Parse<TEnum>(objToParse);
+    }
+    
+    public static IEnumerable<string> GetDisplayNames<T>() where T : struct, Enum
+    {
+        return Enum.GetValues<T>().Select(x => x.GetDisplayName()).ToArray();
+    }
+}
