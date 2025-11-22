4feccc208159d9fac5c157ef2bb2a9989a27c67a
bobby1072 <bobby.thompson1072@gmail.com>
2025-11-22 15:23:31 +0000

    Removed

--- src/BT.Common/BT.Common.OperationTimer/Proto/OperationTimerUtils.cs
+++
@@ -1,7 +0,0 @@
-namespace BT.Common.OperationTimer.Proto
-{
-    /// <summary>
-    /// This class is a prototype for the OperationTimer class
-    /// </summary>
-    public static partial class OperationTimerUtils { }
-}
