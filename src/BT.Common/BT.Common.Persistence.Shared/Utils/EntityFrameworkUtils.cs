d9354434ec18c8cf869afaf37fe9a3a3b5106e0f
bobby1072 <bobby.thompson1072@gmail.com>
2025-11-30 23:21:12 +0000

    Donezo

--- src/BT.Common/BT.Common.Persistence.Shared/Utils/EntityFrameworkUtils.cs
+++ src/BT.Common/BT.Common.Persistence.Shared/Utils/EntityFrameworkUtils.cs
@@ -1,6 +1,4 @@
 using BT.Common.Persistence.Shared.Models;
-using BT.Common.Polly.Extensions;
-using BT.Common.Polly.Models.Abstract;
 using Microsoft.Extensions.Logging;
 
 namespace BT.Common.Persistence.Shared.Utils
@@ -9,22 +7,12 @@ namespace BT.Common.Persistence.Shared.Utils
     {
         public static async Task TryDbOperation(
             Func<Task> dbOperation,
-            ILogger<object>? logger = null,
-            IPollyRetrySettings? pollyRetry = null
+            ILogger<object>? logger = null
         )
         {
             try
             {
-                if (pollyRetry is not null)
-                {
-                    var retryPipe = pollyRetry.ToPipeline();
-                    
-                    await retryPipe.ExecuteAsync(async _ =>  await dbOperation.Invoke());
-                }
-                else
-                {
-                    await dbOperation.Invoke();
-                }
+                await dbOperation.Invoke();
             }
             catch (Exception ex)
             {
@@ -38,19 +26,12 @@ namespace BT.Common.Persistence.Shared.Utils
 
         public static async Task<TResult?> TryDbOperation<TResult>(
             Func<Task<TResult>> dbOperation,
-            ILogger<object>? logger = null,
-            IPollyRetrySettings? pollyRetry = null
+            ILogger<object>? logger = null
         )
             where TResult : DbResult
         {
             try
             {
-                if (pollyRetry is not null)
-                {
-                    var retryPipe = pollyRetry.ToPipeline();
-                    
-                    return await retryPipe.ExecuteAsync(async _ =>  await dbOperation.Invoke());
-                }
                 return await dbOperation.Invoke();
             }
             catch (Exception ex)
@@ -67,19 +48,12 @@ namespace BT.Common.Persistence.Shared.Utils
 
         public static TResult? TryDbOperation<TResult>(
             Func<TResult> dbOperation,
-            ILogger<object>? logger = null,
-            IPollyRetrySettings? pollyRetry = null
+            ILogger<object>? logger = null
         )
             where TResult : DbResult
         {
             try
             {
-                if (pollyRetry is not null)
-                {
-                    var retryPipe = pollyRetry.ToPipeline();
-                    
-                    return retryPipe.Execute(_ => dbOperation.Invoke());
-                }
                 return dbOperation.Invoke();
             }
             catch (Exception ex)
