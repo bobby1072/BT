479fe2a6a4ea01af5d0e41a70c71eb9916cc88fd
BobbyThompson <bobbythompson@paypoint.com>
2026-02-17 17:02:55 +0000

    Explanded the class

--- src/BT.Common/BT.Common.Api.Helpers/Exceptions/ApiException.cs
+++ src/BT.Common/BT.Common.Api.Helpers/Exceptions/ApiException.cs
@@ -9,10 +9,14 @@ public class ApiException: Exception
     public LogLevel LogLevel { get; }
     public HttpStatusCode StatusCode { get; }
     public Dictionary<string, object?> ExtraData { get; set; } = new();
-    public ApiException(LogLevel logLevel, HttpStatusCode statusCode, string message, Exception? innerException = null) : base(message, innerException)
+    public ApiException(LogLevel logLevel, HttpStatusCode statusCode, string message, Exception? innerException = null, Dictionary<string, object?>? extraData = null) : base(message, innerException)
     {
         StatusCode = statusCode;
         LogLevel = logLevel;
+        if (extraData is not null)
+        {
+            ExtraData = extraData;
+        }
     }
 
     public IResult ToResult()
