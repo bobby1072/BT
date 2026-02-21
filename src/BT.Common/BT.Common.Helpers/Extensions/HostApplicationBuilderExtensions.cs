f057c6f8ba25a07b5b46ee4347dc29157a5c49bf
BobbyThompson <bobbythompson@paypoint.com>
2026-02-21 23:19:15 +0000

    Added

---
+++ src/BT.Common/BT.Common.Helpers/Extensions/HostApplicationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace BT.Common.Helpers.Extensions;
+
+public static class HostApplicationBuilderExtensions
+{
+    public static T CheckAndAddSingletonOptions<T>(
+        this IHostApplicationBuilder hostAppBuilder,
+        string? nameofSection = null
+    )
+        where T : class
+    {
+        var sectname = nameofSection ?? typeof(T).Name;
+
+        var configSection = hostAppBuilder.Configuration.GetSection(sectname);
+
+        if (!configSection.Exists())
+        {
+            throw new ArgumentException(sectname);
+        }
+
+        hostAppBuilder.Services.ConfigureSingletonOptions<T>(configSection);
+
+        return configSection.Get<T>() ?? throw new ArgumentException(sectname);
+    }   
+}
